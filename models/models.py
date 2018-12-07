a0bf8ea50f9460b559ddbd1f6b568d1c00223238
Alan Kavishe <alan@filteris.co.za>
2018-12-07 15:00:29 +0200

    Generate invoice from property details

--- models/models.py
+++ models/models.py
@@ -13,6 +13,69 @@ class Property(models.Model):
     estate_id = fields.Many2one("estate.estate", string="Estate")
     invoice_ids = fields.One2many("account.invoice", "property_id", string="Invoices")
 
+    def generate_property_invoice(self):
+        self.ensure_one()
+
+        existing_product = self.env["product.product"].search(
+            [["name", "=", f"Levy: {self.estate_id.name}"]]
+        )
+        if len(existing_product) == 0:
+            product = self.env["product.product"].create(
+                {"name": f"Levy: {self.estate_id.name}", "price": self.levy_amount}
+            )
+        else:
+            product = existing_product[0]
+
+        invoice = self.env["account.invoice"].create(
+            {
+                "type": "out_invoice",
+                "state": "open",
+                "partner_id": self.partner_id.id,
+                "property_id": self.id,
+            }
+        )
+
+        existing_acc_type = self.env["account.account.type"].search([["name", "=", "Levy Account"]])
+        if len(existing_acc_type) == 0:
+            account_type = self.env["account.account.type"].create(
+                {"name": "Levy Account", "type": "receivable", "internal_group": "income"}
+            )
+        else:
+            account_type = existing_acc_type[0]
+
+        existing_account = self.env["account.account"].search([["name", "=", "My Account"]])
+        if len(existing_account) == 0:
+            account = self.env["account.account"].create(
+                {
+                    "name": "My Account",
+                    "code": "MYACC123",
+                    "user_type_id": account_type.id,
+                    "reconcile": True,
+                }
+            )
+        else:
+            account = existing_account[0]
+
+        self.env["account.invoice.line"].create(
+            {
+                "name": f"Levy: {self.estate_id.name}",
+                "invoice_id": invoice.id,
+                "product_id": product.id,
+                "price_unit": self.levy_amount,
+                "account_id": account.id,
+            }
+        )
+
+        return {
+            "name": "Property Invoice Form",
+            "view_type": "form",
+            "view_mode": "form",
+            "target": "new",
+            "res_model": "account.invoice",
+            "type": "ir.actions.act_window",
+            "res_id": invoice.id,
+        }
+
 
 class Estate(models.Model):
     _name = "estate.estate"
